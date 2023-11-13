3dedf8cbc994ad89f8b84acb5c6f80fca1a56e78
ugatai <ugawa.service@gmail.com>
2023-11-13 14:13:35 +0900

    :+1: :feat: 第五章 ハッシュとシンボルの深い理解 #9

--- chapter-five/sample.rb
+++ chapter-five/sample.rb
@@ -109,5 +109,150 @@ buy_burger_set('cheese', **args)
 
 puts '<--------------------------------------------------------------------------------------------------------------->'
 
+currencies = { :japan => 'yen', :us => 'dollar', :india => 'rupee' }
+p currencies.keys
+p currencies.values
+p currencies.has_key?(:japan)
+p currencies.has_key?(:italy)
 
+puts '<--------------------------------------------------------------------------------------------------------------->'
 
+h = { us: 'dollar', :india => 'rupee' }
+currencies = { japan: 'yen', **h }
+p currencies
+
+currencies = { japan: 'yen' }.merge(h)
+p currencies
+
+puts '<--------------------------------------------------------------------------------------------------------------->'
+
+def buy_burger(menu, option = {})
+  drink = option[:drink]
+  potato = option[:potato]
+end
+
+buy_burger('cheese', drink: true, potato: true)
+
+puts '<--------------------------------------------------------------------------------------------------------------->'
+
+def buy_burger_others(menu, drink:, potato:, **others) end
+
+buy_burger_others('cheese', drink: true, potato: true, chicken: false)
+
+params = { drink: true, potato: true, chicken: false }
+# 2.0 ~ 2.7 までは自動で変換が実施されていた
+# buy_burger_others('cheese', params)
+buy_burger_others('cheese', **params)
+
+puts '<--------------------------------------------------------------------------------------------------------------->'
+
+def buy_burger_2(menu, option = {}) end
+
+buy_burger_2('menu', { drink: true, potato: true, chicken: false })
+buy_burger_2('menu', drink: true, potato: true, chicken: false)
+
+def buy_burger_3(option = {}, menu) end
+
+# buy_burger_3(drink: true, potato: true, chicken: false, 'menu')
+buy_burger_3({ drink: true, potato: true, chicken: false }, 'menu')
+
+arr = ['fish', { drink: true, potato: true }]
+puts arr[0]
+puts arr[1]
+
+arr_2 = ['fish', drink: true, potato: true]
+puts arr_2[0]
+puts arr_2[1]
+
+puts '<--------------------------------------------------------------------------------------------------------------->'
+
+# buy_burger_3 { drink: true, potato: true, chicken: false }, 'menu'
+buy_burger_2 'menu', { drink: true, potato: true, chicken: false }
+
+# puts { drink: true, potato: true, chicken: false }
+puts({ drink: true, potato: true, chicken: false })
+
+puts '<--------------------------------------------------------------------------------------------------------------->'
+
+currencies = { japan: 'yen', us: 'dollar', india: 'rupee' }
+p currencies.to_a
+
+arr_3 = [[:japan, "yen"], [:us, "dollar"], [:india, "rupee"]]
+p arr_3.to_h
+
+nums = [1, 2, 3, 4, 5]
+# p nums.to_h
+
+arr_4 = [[:japan, "yen"], [:us, "dollar"], [:japan, "円"]]
+p arr_4.to_h
+
+arr_5 = [[:japan, "yen"], [:us, "dollar"], [:india, "rupee"]]
+puts Hash[arr_5]
+
+puts '<--------------------------------------------------------------------------------------------------------------->'
+
+hash_d = Hash.new('hello')
+a = hash_d[:foo]
+b = hash_d[:bar]
+p a, b
+
+a.upcase!
+p b
+
+# 解決策 => ブロックを使用するようにする
+hash_d = Hash.new { 'hello' }
+
+puts '<--------------------------------------------------------------------------------------------------------------->'
+
+# def bar(*arg, **nil)
+#   p arg
+# end
+#
+# bar({x: 5})
+
+puts '<--------------------------------------------------------------------------------------------------------------->'
+
+name = 'Alice'
+p :"#{name.upcase}"
+p({ key: 'value' })
+
+p %s!ruby is fun!
+p %i(apple, orrage, melon)
+
+puts '<--------------------------------------------------------------------------------------------------------------->'
+
+string = 'apple'
+symbol = :apple
+
+p string.to_sym
+puts string.to_sym == symbol
+
+puts '<--------------------------------------------------------------------------------------------------------------->'
+
+a = nil
+p a&.upcase
+
+id = nil
+id ||= 999
+p id
+
+id_2 = 1
+id_2 ||= 999
+p id_2
+
+# id || id = 999
+
+id_3 = nil
+id_3 ||= begin
+           a = 10
+           b = 10
+           a + b
+         end
+p id_3
+
+# !!find_user return ture or false
+
+p !!true
+p !!nil
+
+puts '<--------------------------------------------------------------------------------------------------------------->'
